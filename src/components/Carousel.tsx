af3eb8c4373b90526ed2fa55dc30a176652005d4
HyunHo Lee <dlgusgh2001@naver.com>
2022-01-18 12:14:28 +0900

    💄캐러샐 미디어 쿼리

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 import { CarouselCard } from "./CarouselCard";
-import { carouselData } from "../constants";
+import { carouselData, MEDIA_QUERY_END_POINT } from "../constants";
+
+type WrapperProps = {
+  curIdx: number;
+  imgSRCLen: number;
+};
 
 type ContainerProps = {
   curIdx: number;
@@ -11,20 +16,20 @@ type ContainerProps = {
 
 export const Carousel = () => {
   const [curIdx, setCurIdx] = useState(0);
-  const [isFlowing, setIsFlowing] = React.useState(true);
+  const [isHover, setIsHover] = useState(true);
   const imgSRCLen = carouselData.length;
 
   useLayoutEffect(() => {
-    let intervalId: any;
-    if (isFlowing) {
+    let intervalId: NodeJS.Timeout;
+    if (isHover) {
       intervalId = setInterval(() => {
         setCurIdx(curIdx + 1);
       }, 3000);
     }
     return () => clearTimeout(intervalId);
-  }, [isFlowing, setCurIdx, curIdx]);
+  }, [isHover, setCurIdx, curIdx]);
 
-  const getStaticIndex = useCallback(
+  const getStaticIdx = useCallback(
     (newID) => {
       let rest = newID % imgSRCLen;
       if (rest < 0) {
@@ -37,17 +42,17 @@ export const Carousel = () => {
 
   return (
     <Container
-      onMouseOver={() => setIsFlowing(false)}
-      onMouseOut={() => setIsFlowing(true)}
+      onMouseOver={() => setIsHover(false)}
+      onMouseOut={() => setIsHover(true)}
     >
-      <CarouselWrapper curIdx={curIdx}>
+      <CarouselWrapper curIdx={curIdx} imgSRCLen={imgSRCLen}>
         <CarouselContainer curIdx={curIdx}>
           {Array(imgSRCLen * 2 + 1)
             .fill(1)
             .map((_, i) => {
               const newID = curIdx + i - imgSRCLen;
               return {
-                staticIdx: getStaticIndex(newID),
+                staticIdx: getStaticIdx(newID),
                 newID,
               };
             })
@@ -71,16 +76,24 @@ export const Carousel = () => {
             })}
         </CarouselContainer>
       </CarouselWrapper>
-      <LeftBtn onClick={() => setCurIdx(curIdx - 1)} aria-label="이전 버튼">
+      <Btn
+        onClick={() => setCurIdx(curIdx - 1)}
+        className="prev"
+        aria-label="이전 버튼"
+      >
         <svg className="SvgIcon_SvgIcon__root__svg__DKYBi" viewBox="0 0 18 18">
           <path d="m6.045 9 5.978-5.977a.563.563 0 1 0-.796-.796L4.852 8.602a.562.562 0 0 0 0 .796l6.375 6.375a.563.563 0 0 0 .796-.796L6.045 9z"></path>
         </svg>
-      </LeftBtn>
-      <RightBtn onClick={() => setCurIdx(curIdx + 1)} aria-label="다음 버튼">
+      </Btn>
+      <Btn
+        onClick={() => setCurIdx(curIdx + 1)}
+        className="next"
+        aria-label="다음 버튼"
+      >
         <svg className="SvgIcon_SvgIcon__root__svg__DKYBi" viewBox="0 0 18 18">
           <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
         </svg>
-      </RightBtn>
+      </Btn>
     </Container>
   );
 };
@@ -88,11 +101,32 @@ export const Carousel = () => {
 const Container = styled.div`
   position: relative;
   overflow: hidden;
-  width: 100%;
+  margin: 25px 0;
+  @media (max-width: ${MEDIA_QUERY_END_POINT.TABLET1}) {
+    height: 300px;
+  }
 `;
 
-const WrapperTransform = ({ curIdx }: ContainerProps) => css`
+const WrapperTransform = ({ curIdx, imgSRCLen }: WrapperProps) => css`
   transform: translateX(${-1885 - 1090 * curIdx}px);
+  @media (max-width: 1600px) {
+    transform: translateX(${-1950 - 1090 * curIdx}px);
+  }
+  @media (max-width: 1530px) {
+    transform: translateX(${-2010 - 1090 * curIdx}px);
+  }
+  @media (max-width: 1386px) {
+    transform: translateX(${-2040 - 1090 * curIdx}px);
+  }
+  @media (max-width: 1330px) {
+    transform: translateX(${-2080 - 1090 * curIdx}px);
+  }
+  @media (max-width: 1280px) {
+    transform: translateX(${-2120 - 1090 * curIdx}px);
+  }
+  @media (max-width: ${MEDIA_QUERY_END_POINT.TABLET1}) {
+    transform: translateX(${-2130 - 1090 * curIdx}px);
+  }
 `;
 
 const CarouselWrapper = styled.div`
@@ -108,39 +142,64 @@ const CarouselContainer = styled.div`
   ${ContainerTransform}
   display: inline-flex;
 `;
-const LeftBtn = styled.button`
+const Btn = styled.button`
   position: absolute;
-  top: 140px;
-  left: 200px;
+  top: 120px;
   height: 60px;
   width: 30px;
   border-radius: 15px;
-  background-color: #dbdbdb;
-  svg {
-    width: 16px;
-    height: 16px;
+  opacity: 0.5;
+  background-color: #fff;
+
+  @media (max-width: ${MEDIA_QUERY_END_POINT.TABLET1}) {
+    display: none;
   }
-`;
-const RightBtn = styled.button`
-  position: absolute;
-  top: 140px;
-  right: 200px;
-  height: 60px;
-  width: 30px;
-  border-radius: 15px;
-  background-color: #dbdbdb;
+
+  &.prev {
+    left: 10vw;
+    @media (max-width: 1500px) {
+      left: 5vw;
+    }
+    @media (max-width: 1386px) {
+      left: 2vw;
+    }
+    @media (max-width: 1280px) {
+      left: 0.5vw;
+    }
+  }
+  &.next {
+    right: 10vw;
+    @media (max-width: 1500px) {
+      right: 5vw;
+    }
+    @media (max-width: 1386px) {
+      right: 3vw;
+    }
+    @media (max-width: 1280px) {
+      right: 0.5vw;
+    }
+  }
   svg {
     width: 16px;
     height: 16px;
   }
 `;
 const CarouselItem = styled.div`
-  margin: 25px 15px;
+  width: 1060px;
+  margin: 0 15px;
 `;
 const ImgCurrent = styled.img`
   border-radius: 5px;
+  @media (max-width: ${MEDIA_QUERY_END_POINT.TABLET1}) {
+    width: 90vw;
+    height: 170px;
+  }
 `;
 const ImgOther = styled.img`
   border-radius: 5px;
   filter: brightness(0.5);
+  @media (max-width: ${MEDIA_QUERY_END_POINT.TABLET1}) {
+    width: 90vw;
+    height: 170px;
+  }
 `;
